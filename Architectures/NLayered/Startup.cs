e74ab78d5c415825ddc4a20d88f873e725d39031
Ivan Srbljinovic <ivan.srbljinovic@outlook.com>
2022-06-17 23:41:44 +0200

    Added swagger

--- Architectures/NLayered/Startup.cs
+++ Architectures/NLayered/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using NLayered.Extensions;
 
 namespace NLayered
@@ -23,6 +24,10 @@ namespace NLayered
             services.ConfigureRepositoryManager();
             services.ConfigureDataServices();
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FirmaApi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +36,8 @@ namespace NLayered
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FirmaApi v1"));
             }
 
             app.UseHttpsRedirection();
